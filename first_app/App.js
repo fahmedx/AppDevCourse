aca16336d619c284b3d2287eaed08219b752b3d1
fahmedx <fahmesalimdx@gmail.com>
2021-01-20 00:28:08 -0300

    Update 19/01/2021

--- first_app/App.js
+++ first_app/App.js
@@ -9,37 +9,21 @@ import Constants from 'expo-constants';
 export default class App extends React.Component{
     constructor(props){
       super(props);
-      this.showText = true;
-      this.showButton = false;
-      this.showBody = true;
-      this.showHeader = true;
+      this.body = new Body();
+      this.body.name = "Joseph";
     }
-
-      showContent(){
-       if(this.showHeader && this.showBody){
-         return(
-           <View>
-             <Header></Header>
-             <Body></Body>
-           </View>
-         );
-       }else {
-         <View>
-           <Text>Empty</Text>
-         </View>
-       }
-      }
-
+    
     render(){
+      console.log(this.body.name);
+
       return(
-          <ScrollView style={{flex:1,backgroundColor:'#343434'}}>           
-            <StatusBar hidden/>
-            {
-              this.showContent()
-            }
-          </ScrollView>
+        <View style={{backgroundColor:'white'}}>
+          <StatusBar hidden/>
+          {this.body.callname()}
+        </View>
       );
     }
+
 }
 
 //const NormalButton = () =>{
@@ -83,3 +67,55 @@ export default class App extends React.Component{
 //            <Text>{Platform.OS}</Text>
 //          </View>
 //        );
+
+
+//mkloop(end){
+        //for(let i=0;i<=end;i++){
+        //  alert(i);
+        //}
+        //or
+        //let i=0;
+        //while(i<=end){
+        //  alert(i);
+        //  i++;
+        //}
+      //} 
+
+//OldCode
+//      mkcomponent(){
+//        for (let i = 0; i <= 10; i++) {
+//          this.component.push(<Text style={{color:'white',fontSize:25}}>Item: {i}</Text>);
+//        }
+//      }
+//      showContent(){
+//       if(this.showHeader && this.showBody){
+//         return(
+//           <View>
+//             <Header></Header>
+//             <Body></Body>
+//             {
+//               this.component.map((val) => {
+//                 return(val);
+//               })
+//             }
+//           </View>
+//         );
+//       }else {
+//         <View>
+//           <Text>Empty</Text>
+//         </View>
+//       }
+//      }
+//
+//    render(){
+//      return(
+//          <ScrollView style={{flex:1,backgroundColor:'#343434'}}>           
+//            <StatusBar hidden/>
+//            {
+//              //this.mkcomponent(),
+//              //this.mkloop(20),
+//              this.showContent()
+//            }
+//          </ScrollView>
+//      );
+//    }
--- first_app/components/Body.js
+++ first_app/components/Body.js
@@ -2,32 +2,52 @@ import React from 'react';
 import {Text, View, ScrollView, Image, StyleSheet} from 'react-native';
 import externStyles from '../styles.js'
 
-const Body = () => {
-    return(
-        <View style={styles.view}>
-          <Image style ={{width:85,height:85}}
-          source={{
-            uri: 'https://reactnative.dev/img/tiny_logo.png'
-          }}
-        />
-        <Text style={externStyles.fontStyle}> Texto</Text>
-        </View>
-    );
+export default class Body extends React.Component{
+  constructor(props){
+    super(props);
+    this.name = "name";
   }
 
-const styles = StyleSheet.create({
-  view:{
-    flex:1,
-    flexDirection:'row',
-    flexWrap:'wrap',
-    padding:30,
-    backgroundColor:'#808080'
-  },
-  text:{
-    width:'70%', 
-    paddingLeft:20,
-    paddingRight:20
-  }  
-});
+  callname(){
+    return(<Text>{this.name}</Text>);
+  }
 
-export default Body;
+  render(){
+    return(
+      <View>
+        <Text>{this.name}</Text>
+      </View>
+    )
+  }
+
+}
+
+//const Body = () => {
+//    return(
+//        <View style={styles.view}>
+//          <Image style ={{width:85,height:85}}
+//          source={{
+//            uri: 'https://reactnative.dev/img/tiny_logo.png'
+//          }}
+//        />
+//        <Text style={externStyles.fontStyle}> Texto</Text>
+//        </View>
+//    );
+//  }
+//
+//const styles = StyleSheet.create({
+//  view:{
+//    flex:1,
+//    flexDirection:'row',
+//    flexWrap:'wrap',
+//    padding:30,
+//    backgroundColor:''
+//  },
+//  text:{
+//    width:'70%', 
+//    paddingLeft:20,
+//    paddingRight:20
+//  }  
+//});
+//
+//export default Body;
